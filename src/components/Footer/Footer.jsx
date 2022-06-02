c4342cf5e26106554f752db762dbf4ad04180845
sk33022 <53243995+sk33022@users.noreply.github.com>
2022-06-02 14:49:09 +0545

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>sunilshrestha910@gmail.com</span>
+        <div className="f-icons">
+          <a href="https://www.instagram.com/suneelstha/"><Insta color="white" size={"3rem"} /></a>
+          <Facebook color="white" size={"3rem"} />
+          <Gitub color="white" size={"3rem"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
